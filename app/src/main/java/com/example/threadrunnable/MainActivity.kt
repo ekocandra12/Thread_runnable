4b6bc9fbc7eecea9671e726e5a740a013ef8a0c4
Eko <yteko12@gmail.com>
2022-04-23 11:09:48 +0700

    Belajar aplikasi mobile, Thread Runnable

---
+++ app/src/main/java/com/example/threadrunnable/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.threadrunnable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Thread(Runnable {
+                killSomeTime()
+            }).start()
+        }
+    }
+
+    private fun killSomeTime(){
+        for (i in 1..20){
+            runOnUiThread(Runnable {
+                textView.text = i.toString()
+            })
+            Thread.sleep(2000)
+            println("i: $i")
+        }
+    }
+}
